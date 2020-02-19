c0a6bd8ab15126310896b4ba36d099a9af961953
kelsyvghn <angelstate@gmail.com>
2020-02-18 21:18:40 -0800

    video function edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,48 @@ var App = () => (
   </div>
 );
 
+// class App extends React.Component {
+//   constructor (props) {
+//     super (props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
+//   render () {
+//     return (
+//       <
+//     )
+//   }
+
+
+
+
+//   render() {
+//     return (
+//       <div>
+//         <nav className="navbar">
+//           <div className="col-md-6 offset-md-3">
+//             <div><h5><em> </em> <Search /></h5></div>
+//           </div>
+//         </nav>
+//         <div className="row">
+//           <div className="col-md-7">
+//             <div><h5><em>videoPlayer</em> <VideoPlayer video={null} /></h5></div>
+//           </div>
+//           <div className="col-md-5">
+//             <div><h5><em>videoList</em> <VideoList /> </h5></div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+
+// }
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
